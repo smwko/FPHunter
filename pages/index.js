06de5348ff2b19b3115afb35194f961592ed543c
smwko <104787885+smwko@users.noreply.github.com>
2022-05-02 23:10:40 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
       <main>
         <Header title="Welcome to my app!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Empieza editando por <code>pages/index.js</code>
         </p>
       </main>
 
